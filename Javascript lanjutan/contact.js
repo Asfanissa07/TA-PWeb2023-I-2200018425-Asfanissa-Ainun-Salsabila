a4848fa1849c08fab015456180ff8a263d881737
Asfanissa Ainun Salsabila <121386693+Asfanissa07@users.noreply.github.com>
2023-06-13 15:59:00 +0700

    javascript lanjutan

---
+++ Javascript lanjutan/contact.js
@@ -0,0 +1,13 @@
+	function submitForm(){
+    		var nama = document.getElementById("nama").value;
+    		var email = document.getElementById("email").value;
+    		var pesan = document.getElementById("pesan").value;
+    		if(nama == "" || email == "" || pesan == ""){
+    			alert("Mohon isikan semua isian form sebelum klik tombol kirim!");
+    		}else{
+    			var hariIni = new Date();
+    			var date = hariIni.toLocalDateString();
+    			alert("Terima kasih! Pesan Anda telah terkirim pada tanggal " + date + ".");
+    		}
+
+    	}
